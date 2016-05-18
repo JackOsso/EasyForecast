80aae0f0b8f0f8dfbb6fa62083c8a0a3a85b3cca
Giacomo Osso <tsam017@tecnicosuperiorekennedy.it>
2016-05-18 18:56:32 +0200

    Reverse Geolocation

--- app/src/main/java/com/example/jack/easyforecast/LocalFragment.java
+++ app/src/main/java/com/example/jack/easyforecast/LocalFragment.java
@@ -1,23 +1,34 @@
 package com.example.jack.easyforecast;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.location.LocationManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.List;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
 
 
 /**
@@ -28,13 +39,15 @@ import android.widget.Toast;
  */
 public class LocalFragment extends Fragment implements LocationListener{
     private static final String TAG = "Localizzazione";
+    private String url = "https://api.forecast.io/forecast/8c83705e47a2b9dd696aa361d375924b/";
     private OnFragmentInteractionListener mListener;
     private  View view;
     private TextView txt;
+    private ImageView img;
     private LocationManager locationManager;
     private double latitude;
     private double longitude;
-    private Location location;
+    private  Location location = null;
 
 
 
@@ -44,36 +57,112 @@ public class LocalFragment extends Fragment implements LocationListener{
         return new LocalFragment();
     }
 
-
+    //APIKEY 8c83705e47a2b9dd696aa361d375924b
+    //from Fahrenheit to Celsius: first subtract 32, then multiply by 100/180
+    //https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE
+    //https://api.forecast.io/forecast/8c83705e47a2b9dd696aa361d375924b/37.8267,-122.423
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_local, container, false);
-
-
+        txt = (TextView) view.findViewById(R.id.locationText);
+        img = (ImageView)view.findViewById(R.id.imageView);
+        //localizzo l'utente
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, this);
 
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-
+        //end
 
 
 
         return view;
     }
 
-
     @Override
     public void onLocationChanged(Location locationC) {
         location = locationC;
         latitude = locationC.getLatitude();
         longitude = locationC.getLongitude();
-        txt = (TextView) view.findViewById(R.id.locationText);
         txt.setText("Lat"+latitude+"long"+longitude);
-
-        Log.d("location: ", location.toString());
+        Log.d(TAG, location.toString());
+        url = url+location.getLatitude()+","+location.getLongitude();
+        Log.d("URL:", url);
+        requestForcast(location);
+        requestCityName(location);
         locationManager.removeUpdates(this);
     }
+
+    //richiesta NomeCitta in cui ci troviamo
+    public void requestCityName(Location location){
+        try {
+
+            Geocoder geo = new Geocoder(getContext(), Locale.getDefault());
+            List<Address> addresses = geo.getFromLocation(latitude, longitude, 1);
+            if (addresses.isEmpty()) {
+                txt.setText("Waiting for Location");
+            }
+            else {
+                if (addresses.size() > 0) {
+                    txt.setText(addresses.get(0).getLocality()+"-"+addresses.get(0).getCountryName());
+                    //Toast.makeText(getApplicationContext(), "Address:- " + addresses.get(0).getFeatureName() + addresses.get(0).getAdminArea() + addresses.get(0).getLocality(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace(); // getFromLocation() may sometimes fail
+        }
+    }
+
+
+    //richiesta Meteo
+    public void requestForcast(Location location){
+        //update request url
+        url = url+location.getLatitude()+","+location.getLongitude();
+        //end
+        img.setBackgroundResource(R.drawable.cloudy);
+        //start request
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        asyncHttpClient.get(url, new AsyncHttpResponseHandler() {
+            @Override //managing response
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Boolean success;
+                try {
+                    JSONObject jsonObject = new JSONObject(new String(responseBody));
+
+
+                    if ((success = jsonObject.getBoolean("success"))) {
+                        //TODO: Parsing JSON OBJ
+
+                    } else {
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(), "Error on jsonObject", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getActivity(), "Error on connection", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
